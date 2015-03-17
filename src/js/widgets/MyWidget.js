b6c1c1582502378c74699ca0281c7c67ccd9f361
Rene Rubalcava <odoe@odoe.net>
2015-03-16 18:57:58 -0700

    added sample using ES6 class for widget

--- src/js/widgets/MyWidget.js
+++ src/js/widgets/MyWidget.js
@@ -1,7 +1,7 @@
 import declare from 'dojo/_base/declare';
 import _WidgetBase from 'dijit/_WidgetBase';
 import Search from 'esri/dijit/Search';
-
+/*
 export default declare([_WidgetBase], {
   postCreate() {
     let map = this.get('map');
@@ -13,3 +13,16 @@ export default declare([_WidgetBase], {
     console.log(i);
   }
 });
+*/
+
+// using ES6 class
+// gets a bit wordy when compiled to ES5
+class MyWidget extends _WidgetBase {
+  postCreate() {
+    let map = this.get('map');
+    this.search = new Search({ map }, 'search');
+    this.search.startup();
+  }
+}
+
+export default MyWidget;
